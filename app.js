666495bfd2a70c4c52868d92951c9c3b21e67976
Micah Bryant <micahbry@umich.edu>
2023-03-02 22:33:23 -0500

    app.js changes

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// Select DOM elements
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const filterRadios = document.querySelectorAll("input[name='filter']");
+const sortSelect = document.getElementById("sortSelect");
+
+// Define tasks array
+let tasks = [];
+
+// Function to display the tasks in the task list
 function displayTasks() {
   taskList.innerHTML = "";
   let filteredTasks = tasks;
@@ -32,3 +43,36 @@ function displayTasks() {
     taskList.appendChild(li);
   });
 }
+
+// Add event listener to the add task button
+addTaskBtn.addEventListener("click", () => {
+  // Get the task text from the input field
+  const taskText = taskInput.value.trim();
+
+  // Create a new task object
+  const task = {
+    text: taskText,
+    completed: false,
+  };
+
+  // Add the task to the tasks array
+  tasks.push(task);
+
+  // Call the displayTasks function to render the tasks
+  displayTasks();
+});
+
+// Add event listeners to the filter radios
+filterRadios.forEach(filterRadio => {
+  filterRadio.addEventListener("change", () => {
+    displayTasks();
+  });
+});
+
+// Add event listener to the sort select
+sortSelect.addEventListener("change", () => {
+  displayTasks();
+});
+
+// Display the tasks when the page loads
+displayTasks();
